79de4b2ef7dad7388857dc139683cf39cbb75e88
常森770 <changsen25@163.com>
2020-08-03 15:38:14 +0800

    删除所有

--- .tea/entryFiles-development/index$.web.js
+++
@@ -1,5 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../client/pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
-require('../../client/pages/first/first?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
